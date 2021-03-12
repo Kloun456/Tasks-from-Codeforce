a935804713effe6219391d9922e8c8d9b1858ca0
Egor <78965361+Kloun456@users.noreply.github.com>
2021-03-12 11:41:54 +0300

    Update Tasks.h

--- Tasks.h
+++ Tasks.h
@@ -8,5 +8,6 @@ template <class X> void Filling_matrix_rand(X** a, int m, int n);
 template <class X> void Filling_matrix_NULL(X** a, int m, int n);
 void competition(const int& n);
 void Lock(int n);
-
+void Azbuka_Borze(char* ch);
+void Egor_Trenning(int n);
 
